2cc18014ffca5cb609cf5ed6793f0ba1ed7c8ea3
XiaomiNote5 <36146385+LamNguy@users.noreply.github.com>
2020-12-31 10:59:25 +0700

    Create backtrack.cpp

---
+++ backtrack.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+// list permutation
+
+int n;
+bool used[100];
+int a[100];
+
+void show(){
+	for ( int i = 1; i<=n ; i++){
+		std::cout<< a[i];
+
+	}
+	std::cout<< std::endl;
+}
+
+void back( int pos ){
+	if ( pos == n+1 ){
+		show();
+		return ;
+	}
+
+	for (int i =1;i <=n ;i++){
+		if ( not used[i]){
+			a[pos]=i;
+			used[i]=true;
+			back(pos+1);
+			used[i]= false;
+		}
+	}
+
+}
+
+
+int main()
+{
+	std:: cin >> n ; 
+	back(1);
+	return 0;
+}
