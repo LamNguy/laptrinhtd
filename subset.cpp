d9f0255e6318fd7b10e4a925960a9857c2c94f4f
XiaomiNote5 <36146385+LamNguy@users.noreply.github.com>
2020-12-31 11:06:34 +0700

    Create subset.cpp

---
+++ subset.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+// subset, backtrack
+
+int n;
+bool a[100];
+
+void show(){
+	for ( int i = 1; i<=n ; i++){
+		if(a[i]) std::cout<< i;
+
+	}
+	std::cout<< std::endl;
+}
+
+void bt( int u ){
+	if ( u == n+1 ){
+		show();
+		return ;
+	}
+
+	a[u]=0;
+	bt(u+1);
+	a[u]=1;
+	bt(u+1);
+
+}
+
+
+int main()
+{
+	std:: cin >> n ; 
+	bt(1);
+	return 0;
+}
